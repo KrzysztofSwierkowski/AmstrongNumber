3dc5c284ddd045a9a548af0d80ea178a57de6ed0
KrzysztofSwierkowski <Swierkowski.K@gmail.com>
2021-11-21 21:09:13 +0100

    Completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static boolean Narcistic(int nNumb){
+        int numberCompare,numberPow,result = 0;
+        numberCompare = nNumb;
+        while (numberCompare !=0){
+            numberPow = numberCompare % 10;
+            result += Math.pow(numberPow,3);
+            numberCompare /= 10;
+        }
+        return result == nNumb;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = scanner.nextInt();
+
+
+        if(Narcistic(number)){
+            System.out.printf("Number %d is Amstrong number %n",number);
+        }
+        System.out.println("Amstrong numbers in range: (Max 1000)");
+        for (int i = 2; i < number ; i++){
+
+            if (Narcistic(i)) {
+                System.out.printf("%d is Amstrong number %n", i);
+            }
+        }
+
     }
 }
